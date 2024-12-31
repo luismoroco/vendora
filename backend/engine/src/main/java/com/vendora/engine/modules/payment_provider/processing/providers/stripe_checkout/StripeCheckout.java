b70619eda07cb8d881816a6683016f09480c6411
lmoroco <lmorocoramos@gmail.com>
2024-12-31 09:42:40 -0500

    feat: add stripe hosted page

--- backend/engine/src/main/java/com/vendora/engine/modules/payment_provider/processing/providers/stripe_checkout/StripeCheckout.java
+++ backend/engine/src/main/java/com/vendora/engine/modules/payment_provider/processing/providers/stripe_checkout/StripeCheckout.java
@@ -1,10 +1,18 @@
 package com.vendora.engine.modules.payment_provider.processing.providers.stripe_checkout;
 
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import com.vendora.engine.common.error.exc.exception.BadRequestException;
 import com.vendora.engine.modules.order.model.Order;
 import com.vendora.engine.modules.payment.model.Payment;
 import com.vendora.engine.modules.payment.model.PaymentMethodType;
 import com.vendora.engine.modules.payment_provider.model.PaymentProvider;
 import com.vendora.engine.modules.payment_provider.processing.providers.PaymentGateway;
+import com.vendora.engine.modules.payment_provider.processing.providers.stripe_checkout.request.StripePaymentInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +26,7 @@ public class StripeCheckout implements PaymentGateway {
   private String SECRET_KEY;
   @Value("${application.payment-providers.stripe-checkout.url}")
   private String URL;
+  Logger LOGGER = LoggerFactory.getLogger(StripeCheckout.class);
 
   private Map<String, String> buildHeaders() {
     return Map.of(
@@ -31,7 +40,35 @@ public class StripeCheckout implements PaymentGateway {
     payment.setPaymentMethodType(PaymentMethodType.CARD);
     payment.setPaymentProvider(PaymentProvider.STRIPE_CHECKOUT);
 
-    return payment;
+    if (!args.containsKey("args")) {
+      throw new BadRequestException("Required parameters not provided");
+    }
+
+    var paymentInformation = (StripePaymentInformation) args.get("args");
+
+    Stripe.apiKey = this.SECRET_KEY;
+    var sessionParams = SessionCreateParams.builder()
+      .setSuccessUrl(paymentInformation.getRedirectUrl())
+      .setCancelUrl(paymentInformation.getRedirectUrl())
+      .putMetadata("externalReference", order.getOrderId().toString())
+      .addLineItem(
+        SessionCreateParams.LineItem.builder()
+          .setName("Order %d".formatted(order.getOrderId()))
+          .setAmount(fixAmount(order.getAmount()))
+          .setCurrency(order.getCurrency().name())
+          .setQuantity(1L)
+          .build()
+      )
+      .setMode(SessionCreateParams.Mode.PAYMENT)
+      .build();
+
+    try {
+      var session = Session.create(sessionParams);
+      return payment;
+    } catch (StripeException e) {
+      LOGGER.error("Error in stripe initialization payment [orderId=%d][error=%s]".formatted(order.getOrderId(), e));
+      throw new RuntimeException("Error in payment");
+    }
   }
 
   @Override
@@ -41,6 +78,10 @@ public class StripeCheckout implements PaymentGateway {
 
   @Override
   public void checkConfiguration() {
-    System.out.println("Hola desde el proveedor de stripe xd");
+  }
+
+  private static Long fixAmount(Double amount) {
+    Double fixedAmount = amount * 100;
+    return fixedAmount.longValue();
   }
 }
